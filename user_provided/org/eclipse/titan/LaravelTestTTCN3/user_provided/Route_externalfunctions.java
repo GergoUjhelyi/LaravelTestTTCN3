63daba3743569488a07e14d9558d6e01b9a89101
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-01-14 16:00:24 +0100

    typo

--- user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Route_externalfunctions.java
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Route_externalfunctions.java
@@ -8,7 +8,7 @@ public class Route_externalfunctions {
 
 	public static Route__Obj__List f__readRoutesFile(TitanCharString f__filename) {
 		if (!f__filename.is_bound() || f__filename.get_value().isEmpty()) {
-			//IllegalArgumentException would be better, but for testing an empty list is better
+			//IllegalArgumentException would be good, but for testing an empty list is better
 			return new Route__Obj__List(TitanNull_Type.NULL_VALUE);
 		}
 		// TODO Auto-generated method stub
