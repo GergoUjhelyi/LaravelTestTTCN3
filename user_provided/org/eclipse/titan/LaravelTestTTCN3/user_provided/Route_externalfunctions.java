99a8d6b2dd1cd9671bb91ce69f24bf8f2b3d7b20
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-01-14 15:53:39 +0100

    create init tests file

---
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Route_externalfunctions.java
@@ -0,0 +1,18 @@
+package org.eclipse.titan.LaravelTestTTCN3.user_provided;
+
+import org.eclipse.titan.LaravelTestTTCN3.generated.Route.Route__Obj__List;
+import org.eclipse.titan.runtime.core.TitanCharString;
+import org.eclipse.titan.runtime.core.TitanNull_Type;
+
+public class Route_externalfunctions {
+
+	public static Route__Obj__List f__readRoutesFile(TitanCharString f__filename) {
+		if (!f__filename.is_bound() || f__filename.get_value().isEmpty()) {
+			//IllegalArgumentException would be better, but for testing an empty list is better
+			return new Route__Obj__List(TitanNull_Type.NULL_VALUE);
+		}
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
