08ca370c11ebdcd63b5af8a3152142ace2339599
Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>
2022-02-16 21:23:25 +0100

    create init_functions external file
    Signed-off-by: Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>

---
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Init__Functions_externalfunctions.java
@@ -0,0 +1,17 @@
+package org.eclipse.titan.LaravelTestTTCN3.user_provided;
+
+import org.eclipse.titan.runtime.core.TitanBoolean;
+import org.eclipse.titan.runtime.core.TitanCharString;
+
+public class Init__Functions_externalfunctions {
+
+	public static TitanBoolean ef__valid__path(TitanCharString serverDirectory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void ef__create__route__file(TitanCharString serverDirectory, TitanCharString routeFile) {
+		// TODO Auto-generated method stub
+	}
+
+}
