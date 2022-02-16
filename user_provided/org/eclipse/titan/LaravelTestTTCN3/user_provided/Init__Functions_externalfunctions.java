955a635b6f5a10cfc717c855110e7bfea70260c3
Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>
2022-02-16 22:30:51 +0100

    finish external function
    Signed-off-by: Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>

--- user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Init__Functions_externalfunctions.java
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Init__Functions_externalfunctions.java
@@ -1,5 +1,7 @@
 package org.eclipse.titan.LaravelTestTTCN3.user_provided;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
@@ -22,8 +24,30 @@ public class Init__Functions_externalfunctions {
 		return new TitanBoolean(true);
 	}
 
+	//Create route file
 	public static void ef__create__route__file(TitanCharString serverDirectory, TitanCharString routeFile) {
-		// TODO Auto-generated method stub
+		if (serverDirectory == null || !serverDirectory.is_bound()) {
+			throw new TtcnError(new IllegalArgumentException("Server directory must be bound!"));
+		}
+		if (routeFile == null || !routeFile.is_bound()) {
+			throw new TtcnError(new IllegalArgumentException("Route file must be bound!"));
+		}
+		
+		final boolean isAbsolute = Path.of(serverDirectory.get_value().toString()).isAbsolute();
+		final String directory = isAbsolute ? serverDirectory.get_value().toString() : System.getProperty("user.dir") + serverDirectory.get_value().toString();
+		final boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+		
+		ProcessBuilder builder = new ProcessBuilder();
+		if (isWindows) {
+			builder.command("cmd.exe", "/c", String.format("php artisan route:list --json > %s/%s", System.getProperty("user.dir"), routeFile));
+		} else {
+			builder.command("sh", "-c", String.format("php artisan route:list --json > %s", routeFile));
+		}
+		builder.directory(new File(directory));
+		try {
+			Process process = builder.start();
+		} catch (IOException e) {
+			throw new TtcnError(e);
+		}
 	}
-
 }
