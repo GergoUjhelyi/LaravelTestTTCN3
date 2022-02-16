d44f0f13eaf352e697a3398c9b3fd21f7678276c
Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>
2022-02-16 21:46:51 +0100

    valid path function
    Signed-off-by: Gergo Ujhelyi <ujhelyi.gergo@ericsson.com>

--- user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Init__Functions_externalfunctions.java
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/Init__Functions_externalfunctions.java
@@ -1,13 +1,24 @@
 package org.eclipse.titan.LaravelTestTTCN3.user_provided;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
 import org.eclipse.titan.runtime.core.TitanBoolean;
 import org.eclipse.titan.runtime.core.TitanCharString;
+import org.eclipse.titan.runtime.core.TtcnError;
 
 public class Init__Functions_externalfunctions {
 
 	public static TitanBoolean ef__valid__path(TitanCharString serverDirectory) {
-		// TODO Auto-generated method stub
-		return null;
+		if (serverDirectory == null || !serverDirectory.is_bound()) {
+			throw new TtcnError(new IllegalArgumentException("Server directory must be bound!"));
+		}
+		try {
+			final Path path = Path.of(serverDirectory.get_value().toString());
+		} catch (InvalidPathException e) {
+			return new TitanBoolean(false);
+		}
+		return new TitanBoolean(true);
 	}
 
 	public static void ef__create__route__file(TitanCharString serverDirectory, TitanCharString routeFile) {
