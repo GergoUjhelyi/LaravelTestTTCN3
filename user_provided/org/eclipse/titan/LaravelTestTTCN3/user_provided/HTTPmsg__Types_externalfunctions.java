7ae0ab0ad6890e0ae9d17c2f58973232cc3e3031
Ujhelyi Gergő <gergo.ujhelyi@nokia.com>
2022-08-21 20:39:52 +0200

    http external function comments

--- user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/HTTPmsg__Types_externalfunctions.java
+++ user_provided/org/eclipse/titan/LaravelTestTTCN3/user_provided/HTTPmsg__Types_externalfunctions.java
@@ -8,35 +8,36 @@ import org.eclipse.titan.runtime.core.TitanBoolean;
 import org.eclipse.titan.runtime.core.TitanInteger;
 import org.eclipse.titan.runtime.core.TitanOctetString;
 
-//=========================================================================
-//==== Encoder-decoder Functions independent from sending and receiving:===
-//=========================================================================
+/**
+ * 
+ * Encoder-decoder Functions independent from sending and receiving.
+ * 
+ * @author gujhelyi
+ */
 public class HTTPmsg__Types_externalfunctions {
 
 	/*********************************************************
-	* Function: enc__HTTPMessage
-	*
-	* Purpose:
-	*    To encode msg type of HTTPMessage into OCTETSTRING separated from sending functionality
-	*    It is for users using this test port as a protocol module
-	*
-	* References:
-	*   RFC2616
-	*
-	* Precondition:
-	*  msg is filled in properly
-	* Postcondition:
-	*
-	*
-	* Parameters:
-	*  msg - the HTTP Message to be encoded
-	*
-	* Return Value:
-	*   OCTETSTRING - the encoded message
-	* Detailed Comments:
-	*   -
-	*
-	*********************************************************/
+	 * Function: enc__HTTPMessage
+	 *
+	 * Purpose:
+	 *    To encode msg type of HTTPMessage into OCTETSTRING separated from sending functionality
+	 *    It is for users using this test port as a protocol module
+	 *
+	 * References:
+	 *   RFC2616
+	 *
+	 * Precondition:
+	 *  msg is filled in properly
+	 * Postcondition:
+	 *
+	 * Detailed Comments:
+	 * -
+	 *
+	 * @param msg - the HTTP Message to be encoded
+	 *
+	 * @return OCTETSTRING - the encoded message
+	 *
+	 *********************************************************/
 	public static TitanOctetString enc__HTTPMessage(HTTPMessage msg) {
 		TTCN_Buffer buf = new TTCN_Buffer();
 		buf.clear();
@@ -44,35 +45,35 @@ public class HTTPmsg__Types_externalfunctions {
 		return new TitanOctetString(buf.get_data());
 	}
 	/*********************************************************
-	* Function: dec__HTTPMessage
-	*
-	* Purpose:
-	*    To decode msg type of OCTETSTRING into HTTPMessage separated from receiving functionality
-	*    It is for users using this test port as a protocol module
-	*
-	* References:
-	*   RFC2616
-	*
-	* Precondition:
-	*  stream is filled in properly
-	* Postcondition:
-	*  -
-	*
-	* Parameters:
-	*  stream - the message to be decoded
-	*  msg    - reference to the record type of HTTPMessage which will contain the decoded value if the return value less than the length of the original stream
-	* Return Value:
-	*   integer - the length of the remaining data which is not decoded yet.
-	* Detailed Comments:
-	*   If the full stream is decoded, the return value is zero
-	*   If nothing is decoded (decoding failed) the return value equals to the original length of the stream
-	*
-	*********************************************************/
+	 * Function: dec__HTTPMessage
+	 *
+	 * Purpose:
+	 *    To decode msg type of OCTETSTRING into HTTPMessage separated from receiving functionality
+	 *    It is for users using this test port as a protocol module
+	 *
+	 * References:
+	 *   RFC2616
+	 *
+	 * Precondition:
+	 *  stream is filled in properly
+	 * Postcondition:
+	 *  -
+	 *  
+	 * Detailed Comments:
+	 * 	If the full stream is decoded, the return value is zero
+	 * 	If nothing is decoded (decoding failed) the return value equals to the original length of the stream
+	 * 
+	 * @param stream - the message to be decoded
+	 * @param msg - reference to the record type of HTTPMessage which will contain the decoded value if the return value less than the length of the original stream
+	 * @param socket_debugging - boolean value to provide debug logging messages.
+	 * 
+	 * @return integer - the length of the remaining data which is not decoded yet.
+	 *********************************************************/
 	public static TitanInteger dec__HTTPMessage(TitanOctetString stream, HTTPMessage msg, TitanBoolean socket_debugging) {
 		TTCN_Logger.log(Severity.DEBUG_TESTPORT, "starting HTTPmsg__Types.dec__HTTPMessage");
 		TTCN_Buffer buf_p = new TTCN_Buffer();
 		buf_p.put_os(stream);
-		
+
 		int buf_len = buf_p.get_read_len();
 		if (buf_len > 0) {
 			if (HTTPmsg__PT.f_HTTP_decodeCommon(buf_p, msg, true, socket_debugging.get_value(), null, null)) {
