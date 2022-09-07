266e7962f1640cd8f25e6f4632554ba03c9b9306
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2022-09-07 20:54:29 +0200

    add empty unit test

---
+++ unit_tests/org/eclipse/titan/LaravelTestTTCN3/unitTests/HTTPMsgUnitTests.java
@@ -0,0 +1,14 @@
+package org.eclipse.titan.LaravelTestTTCN3.unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HTTPMsgUnitTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
